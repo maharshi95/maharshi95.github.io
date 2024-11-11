85320aee5bd1f2d06cb2b7215600af6aa35ae50c
Maharshi Gor <maharshigor@gmail.com>
2024-11-11 00:03:18 -0500

    EMNLP 2024 Updates

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-  // add toggle functionality to abstract and bibtex buttons
+  // add toggle functionality to abstract, bibtex, tldr and video buttons
   $('a.abstract').click(function() {
     $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
   });
   $('a.bibtex').click(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+  });
+  $('a.tldr').click(function() {
+    $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+  });
+  $('a.video').click(function() {
+    $(this).parent().parent().find(".video.hidden").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
   });
   $('a').removeClass('waves-effect waves-light');
 
